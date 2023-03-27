7c673e7879eb32e9487e085c3bbafe56c2bb1ca3
jonathan bola <bolajonathan@yahoo.com>
2023-03-27 13:38:34 -0500

    creating components

---
+++ client/src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+        <div>
+            <h1>Front-End React Developer</h1>
+            <p>Hi, my name is Jonathan Bola. A passionate Front-End 
+                React Developer based in Fort Worth, TX</p>
+                <i></i>
+                <i></i>
+
+        </div>
+        <div>
+            <img/>
+        </div>
+
+        Tech Stack |
+    </div>
+  )
+}
