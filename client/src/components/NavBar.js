48e57559571bb361fcf517c3b518cb22acb224bd
jonathan bola <bolajonathan@yahoo.com>
2023-03-30 13:36:17 -0500

    components styling

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,81 +6,25 @@ import '../styles/Navbar.css'
 
 export default function NavBar() {
   return (
-    <div className=' menu-container fixed-top'>
-      <div><nav className="container">
-
-    <input type="checkbox" aria-label="Toggle menu" />
-    <span></span>
-    <span></span>
-    <span></span>
-  
-
-    
-
-    <div className="menu">
-      <a href="/" class="menu-logo">
-      <h1>jonbola.dev</h1>
-      </a>
-  
-      <ul>
-        <li> <a
-        to="/"
-        exact
-        activeStyle={{
-            color: "#599bb3",
-            
-        }}
-        >
-        HOME
-        </a>
-        </li>
-    
-        <li><a
-        to="/about"
-        exact
-        activeStyle={{
-            color: "#599bb3",
-        }}
-        >
-        ABOUT
-        </a></li>
+    <div className=''>
+                <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top
+                shadow p-3 mb-5 bg-body-tertiary rounded">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Jonbola.dev</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <a className="nav-link" aria-current="page" href="#">HOME</a>
+                <a className="nav-link" href="#">ABOUT</a>
+                <a className="nav-link" href="#">PROJECTS</a>
+                <a className="nav-link">CONTACT</a>
+              </div>
+            </div>
+          </div>
+        </nav>
       
-        <li>
-            <a
-            to="/"
-            exact
-            activeStyle={{
-                color: "#599bb3",
-            }}
-            >
-           PROJECTS
-           </a>
-      </li>
-      <li>
-            <a
-            to="/footer"
-            onClick={()=>{
-              window.scrollTo({
-                top: 1000,
-                behavior: "smooth"
-              })
-            }
-            }
-            exact
-            activeStyle={{
-                color: "#599bb3",
-            }}
-            >
-           CONTACT
-           </a>
-      </li>
-   
-    </ul>
-
-   
-    </div>
-  </nav>
-  </div>
   </div>
   )
 }
